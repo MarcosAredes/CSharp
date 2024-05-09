9855a2db43b0044d6c26266e590f12b0ace7b395
MarcosAredes <Ducarx40@hotmail.com>
2024-05-09 01:15:52 -0300

    uai

---
+++ Fundamentals/03TiposDados/varNumber.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = 20;
+        long num2 = 7896541230;
+        short num3 = -10;
+        byte num4 = 128;
+        float num5 = 1.75f;
+        double num6 = 3.14159265359;
+        decimal num7 = 19.99m;
+
+        Console.WriteLine("int\t" + num1);
+        Console.WriteLine("long\t" + num2);
+        Console.WriteLine("short\t" + num3);
+        Console.WriteLine("byte\t" + num4);
+        Console.WriteLine("float\t" + num5);
+        Console.WriteLine("double\t" + num6);
+        Console.WriteLine("decimal\t" + num7);
+    }
+}
